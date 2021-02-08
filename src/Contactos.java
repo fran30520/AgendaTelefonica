a5b497547eace0a52c53b1d6dfa054ca68191ce2
fran30520 <fran30520@icloud.com>
2021-02-08 21:40:25 +0100

    creo la clase contactos

--- src/Contactos.java
+++ src/Contactos.java
@@ -1,2 +1,35 @@
 public class Contactos {
+
+    //variables y constantes
+    private String nombre;
+    private int numero;
+
+    //constructor
+    public Contactos(String nombre, int numero) {
+        this.nombre = nombre;
+        this.numero = numero;
+    }
+
+    //metodos Getter y Setter
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    //metodo toString
+    public String toString(){
+        return "El " + nombre + "tiene el número de telefono: " + numero;
+    }
 }
