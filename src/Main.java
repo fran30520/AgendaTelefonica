c93a420d149e56cf03870a6586d72a4662e2b7f3
fran30520 <fran30520@icloud.com>
2021-02-12 23:08:48 +0100

    segunda correccion del main

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-import javax.security.sasl.SaslClient;
+
 import java.util.Scanner;
 
 public class Main {
@@ -6,10 +6,13 @@ public class Main {
     public static void main(String[] args) {
         //Creamos objeto Agenda para hacer todo tipo de movimientos en ella
         Agenda agenda = new Agenda();
+        Contactos contacto;
 
         //variables
-        int opcion=0;
         Scanner sc = new Scanner(System.in);
+        int opcion = 0;
+        String nombre = "";
+        int telefono = 0;
 
         System.out.println("bienvenidos a tu Agenda");
         System.out.println("-----------------------");
@@ -17,14 +20,42 @@ public class Main {
 
         do {
 
-            System.out.println("Introducir contacto nuevo introduce 1");
-            System.out.println("Buscar contacto por nombre introduce 2");
-            System.out.println("Eliminar contacto introduce 3");
-            System.out.println("Listar todos los contactos introduce 4");
-            System.out.println("Para salir introduce 0");
+            System.out.println("Introducir contacto nuevo: introduce 1");
+            System.out.println("Buscar contacto por nombre: introduce 2");
+            System.out.println("Eliminar contacto: introduce 3");
+            System.out.println("Listar todos los contactos: introduce 4");
+            System.out.println("Para salir: introduce 0");
             opcion = sc.nextInt();
 
+            switch (opcion) {
+                case 1:
+                    System.out.println("introduce un nombre");
+                    nombre = sc.nextLine();
+                    System.out.println("introduce el telefono");
+                    sc.nextInt();
 
+                    contacto = new Contactos(nombre, telefono);
+
+                    agenda.aniadirContacto(contacto);
+                    break;
+                case 2:
+                    System.out.println("Introduce el contacto que quieras buscar");
+                    nombre = sc.nextLine();
+
+                    agenda.buscaContacto(nombre);
+                    break;
+                case 3:
+
+                    agenda.eliminarContacto();
+                    break;
+
+                case 4:
+                    agenda.listarContactos();
+                    break;
+
+                case 0:
+                    break;
+            }
 
         } while (opcion != 0);
 
